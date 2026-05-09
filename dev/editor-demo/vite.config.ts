e02243ce2de5a46618b05ce32fe41eed1390eb23
赵俊哲 <1429595365@qq.com>
2026-05-09 19:48:07 +0800

    feat: demo部分打包设置更新 & 类型文件更新

--- dev/editor-demo/vite.config.ts
+++ dev/editor-demo/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 //import { fileURLToPath, URL } from "node:url";
 import fs from "fs";
@@ -28,9 +28,22 @@ const alias = [
 ];
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({command}) => ({
+  base:command === 'build' ? 'editor' : '/',
   plugins: [react()],
+  build:{
+    outDir:"editor"
+  },
   resolve: {
     alias: alias,
   },
-})
+  server:{
+    proxy:{
+      "/api":{
+        target:"http://localhost:3001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
+}))
--- packages/editor-context/src/types/imageProps.ts
+++ packages/editor-context/src/types/imageProps.ts
@@ -1,3 +1,31 @@
+interface UploadProgressEvent extends Partial<ProgressEvent> {
+  percent?: number;
+}
+export interface RcFile extends File {
+  uid: string;
+}
+export type UploadRequestMethod =
+  | 'POST'
+  | 'PUT'
+  | 'PATCH'
+  | 'post'
+  | 'put'
+  | 'patch';
+export interface UploadRequestError extends Error {
+  status?: number;
+  method?: UploadRequestMethod;
+  url?: string;
+}
+export type UploadRequestFile =
+  | Exclude<BeforeUploadFileType, File | boolean>
+  | RcFile;
+export type BeforeUploadFileType = File | Blob | boolean | string;
+export interface IImagePropsUploadOption<T = any> {
+  file: UploadRequestFile;
+  onProgress?: (event: UploadProgressEvent, file?: UploadRequestFile) => void;
+  onError?: (event: UploadRequestError | ProgressEvent, body?: T) => void;
+  onSuccess?: (body: T, fileOrXhr?: UploadRequestFile | XMLHttpRequest) => void;
+}
 export type IImageProps = {
   max: number;
   minWidth: number;
@@ -6,6 +34,6 @@ export type IImageProps = {
   onImageBeforeUpload: (file: File, fileList: File[]) => boolean;
   onImageStartUpload: (fileList: File[]) => void;
   onImageEndUpload: (success: boolean, files?: File[]) => void;
-  onImageUpload: (options: any) => void;
+  onImageUpload: (options: IImagePropsUploadOption) => void;
   onImagePaste: (url: string) => Promise<{ data: { id: string; url: string } }>;
 };
